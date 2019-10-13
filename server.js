d006c13efc255c088d85265545a31f0a0500a644
waterwatermelon <32012200+waterwatermelon@users.noreply.github.com>
2019-10-13 11:23:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-//搭建一个简单本地服务器
+// 搭建一个简单本地服务器
 var http=require('http');
-var fs = require('fs');//引入fs模块来读取文件
-var root = "E:/Web前端训练营/第一阶段/第一阶段任务代码" //项目根目录
+var fs = require('fs'); // 引入fs模块来读取文件
+var root = "E:/Web前端训练营/第一阶段/第一阶段任务代码" // 项目根目录
 var server = http.createServer(function(req,res){
     var url = req.url;
     var file = root + url;
